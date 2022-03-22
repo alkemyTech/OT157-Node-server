82bf82dcfd1301de3d245cfb151a681bcd083373
Facundo <facundolopezcrespo@hotmail.com>
2022-03-22 11:40:22 -0300

    change two files:testimonials controller and testimonial model

--- controllers/testimonialsController.js
+++ controllers/testimonialsController.js
@@ -2,10 +2,29 @@ const db=require("../models/index")
 const testimonialsControler={
     list:(req,res)=>{
         db.testimonial.findAll()
-        .then(results=>{
-            res.send("próximamente se renderizará el resultado de la respectiva búsqueda en la DB")
+        .then(testimonials=>{
+            let response={
+                meta:{
+                    status:200,
+                    count:testimonials.length,
+                    url:"/testimonials"
+                },
+                data:testimonials
+            }
+            res.json(response)
+            
         })
-        .catch()
+        .catch(error=>{
+            console.log(error);
+            let response={
+                meta:{
+                    status:500,
+                    statusMessage:"Error server"
+                },
+                data:[]
+            }
+            res.json(response)
+        })
     }
 }
 
