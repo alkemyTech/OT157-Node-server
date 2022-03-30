0b20fc0dd481064b81effd519096a2d78a34bbe7
franmassello <franmassello@gmail.com>
2022-03-29 23:10:01 -0300

    add update

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -1,44 +1,15 @@
-require("dotenv").config();
-const sgMail = require('@sendgrid/mail')
-// Esta funcion recibe un email y le manda el mail de bienvenida, la funcioon se deberia llamar 
-// cuando se registra un nuevo usuario. 
-// Esta hecho usando SendGrid, me registre y genere una API key, cree un verified sender (el que esta en el .env)
-// y tambien cargue el template del mail a SendGrid. Este template es dinamico, es decir le podes pasar variables
-// por ej: algun nombre, alguna fecha, etc. En este caso le pase el titulo, el texto y el contact data.
+const emailFunctions = require('../services/emails');
 
-function sendWelcomeEmail (to) {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-    const msg = {
-        to: to,
-        from: process.env.SENDGRID_SENDER,
-        templateId: process.env.SENDGRID_TEMPLATE_ID,   
-        dynamic_template_data: {
-            title: "Bienvenido a nuestra ONG",
-            email_text: "Gracias por unirte a nuestra comunidad",
-            contact_data:"Contacto: alkemy.node157@gmail.com"
-        }
+//emailFunctions.sendWelcomeEmail("franmassello7670@gmail.com");
+//emailFunctions.sendDefaultEmail("franmassello7670@gmail.com", "Test", "Test");
+const emailsController = {	
+    sendWelcomeEmail: async (req, res) => {
+        await emailFunctions.sendWelcomeEmail(req.body.email);
+        res.status(200).json({ msg: "Email sent" });
+    },
+    sendDefaultEmail: async (req, res) => {
+        await emailFunctions.sendDefaultEmail(req.body.email, req.body.subject, req.body.text);
+        res.status(200).json({ msg: "Email sent" });
     }
-    sgMail.send(msg)
-};
-
-function sendDefaultEmail(to, subject, text) {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-    const msg = {
-        to: to,
-        from: process.env.SENDGRID_SENDER,
-        subject: subject,
-        text: text
-    }
-    sgMail.send(msg)
 }
-
-// sendWelcomeEmail("franmassello7670@gmail.com") Linea para testear la funcion, 
-// sendDefaultEmail("franmassello7670@gmail.com", "Test", "Test") 
-
-
-
-
-module.exports = {
-    sendWelcomeEmail,
-    sendDefaultEmail
-}
+module.exports = emailsController;
