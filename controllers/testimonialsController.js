f7bce9a9452f74b408036b7e51dceb8efc68139e
Facundo <facundolopezcrespo@hotmail.com>
2022-04-05 16:06:37 -0300

    add update method for testimonial model

--- controllers/testimonialsController.js
+++ controllers/testimonialsController.js
@@ -1,5 +1,6 @@
 const testimonialsService=require("../services/testimonialsService")
 const {validationResult}=require("express-validator")
+
 const testimonialsControler={
     list:async (req,res)=>{
         try {
@@ -14,6 +15,7 @@ const testimonialsControler={
             }
             res.status(200).json(response)
         } catch (error) {
+            console.log(error)
             let response={
                 meta:{
                     status:500,
@@ -28,10 +30,16 @@ const testimonialsControler={
         const validation=validationResult(req)
         const errors=validation.mapped()
         if (!validation.isEmpty()) {
-            let response={}
-            if (errors.name) {response.nameError=errors.name.msg}
-            if (errors.content) {response.contentError=errors.content.msg}
-            res.json(response)
+            let response={
+                meta:{
+                    status:400,
+                    statusMessage:"Error del cliente"
+                },
+                Error:[]
+            }
+            if (errors.name) {response.Error.push(errors.name.msg) }
+            if (errors.content) {response.Error.push(errors.content.msg)}
+            res.status(400).json(response)
         } else {
            try {
                 const testimonial=req.body
@@ -50,13 +58,71 @@ const testimonialsControler={
                 let response={
                     meta:{
                         status:500,
-                        statusMessage:"Error server"
+                        statusMessage:"Error multiple server"
                     },
                     data:[]
                 }
                 res.status(500).json(response)
             } 
         }
+    },
+    update:async(req,res)=>{
+        const validation=validationResult(req)
+        const errors=validation.mapped()
+        try{
+            const testimonialUpdating=await testimonialsService.findByPk(req.params.id)
+            if (testimonialUpdating && validation.isEmpty()) {
+                const countRowUpdate=await testimonialsService.update(req.body,req.params.id)
+                const testimonialUpdate=await testimonialsService.findByPk(req.params.id)
+                let response={
+                    meta:{
+                        status:200,
+                        countRowsUpdate:countRowUpdate,
+                        url:"/testimonials/id"
+                    },
+                    data:testimonialUpdate
+                }
+                res.status(200).json(response)
+            } else if (testimonialUpdating==null) {
+                let response={
+                    meta:{
+                        status:400,
+                        statusMessage:"Error del cliente"
+                    },
+                    Error:'No existe el testimonio que se quiere actualizar'
+                }
+                res.status(400).json(response)
+            } else {
+                let response={
+                    meta:{
+                        status:400,
+                        statusMessage:"Error del cliente"
+                    },
+                    Error:[]
+                }
+                if (errors.name) {response.Error.push(errors.name.msg) }
+                if (errors.content) {response.Error.push(errors.content.msg)}
+                res.status(400).json(response)
+            }
+
+        } catch(error) {
+            console.log(error)
+        }
+        
+        /* const validation=validationResult(req)
+        const errors=validation.mapped()
+        if (!validation.isEmpty()) {
+            let response={
+                meta:{
+                    status:400,
+                    statusMessage:"Error del cliente"
+                },
+                Error:[]
+            }
+            if (errors.name) {response.Error.push(errors.name.msg) }
+            if (errors.content) {response.Error.push(errors.content.msg)}
+            res.status(400).json(response)
+        } */
     }
 }
 module.exports=testimonialsControler
--- routes/testimonials.js
+++ routes/testimonials.js
@@ -4,5 +4,6 @@ const testimonialsControler=require("../controllers/testimonialsController")
 const validateTestimonial=require("../middlewares/validateTestimonial")
 router.get("/",testimonialsControler.list)
 router.post("/",validateTestimonial,testimonialsControler.create)
+router.put("/:id",validateTestimonial,testimonialsControler.update)
 
 module.exports=router
