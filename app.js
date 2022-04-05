acb384a783dd473243727da7b9769a4f0b682694
Facundo <facundolopezcrespo@hotmail.com>
2022-04-05 10:23:28 -0300

    add the admin's route

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ app.use(cors());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// express-session
+const session=require("express-session")
+app.use(session({secret:"It is a secret!"}))
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
--- routes/testimonials.js
+++ routes/testimonials.js
@@ -2,7 +2,8 @@ const express=require("express")
 const router=express.Router()
 const testimonialsControler=require("../controllers/testimonialsController")
 const validateTestimonial=require("../middlewares/validateTestimonial")
+const protegerRutas=require("../middlewares/protegerRutsas")
 router.get("/",testimonialsControler.list)
-router.post("/",validateTestimonial,testimonialsControler.create)
+router.post("/",protegerRutas,validateTestimonial,testimonialsControler.create)
 
 module.exports=router
