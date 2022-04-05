6c88a948ce25efbc1b90a68da570de82007fa96b
Anarktos <camilowhite7@gmail.com>
2022-04-05 15:06:26 -0500

    GET category detail and DELETE category

--- routes/categories.js
+++ routes/categories.js
@@ -1,12 +1,11 @@
-const { getCategoriesList, createCategory, getCategoryDetail, deleteCategory} = require('../controllers/categoriesController');
+const { getCategoriesList, createCategory, getCategoryDetail, deleteCategory } = require('../controllers/categoriesController');
 const express = require('express');
 const router = express.Router();
 const { validateCreate } = require('../validators/categoriesValidator');
-const adminMiddleware = require('../middlewares/adminMiddleware');
-const authMiddleware = require('../middlewares/authMiddleware');
 
 
-router.get('/', adminMiddleware, getCategoriesList);
+
+router.get('/', getCategoriesList);
 router.get('/:id', getCategoryDetail);
 router.post('/', validateCreate, createCategory);
 router.delete('/:id', deleteCategory);
