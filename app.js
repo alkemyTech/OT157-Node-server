14f8711bb0a7bf7a6e7ee6972d5f5d294a0b3f51
FedericoMelgarejo <f.n.melgarejo@gmail.com>
2022-04-05 21:11:30 -0300

    added create and get by id methods for News resource

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const categoriesRouter = require('./routes/categories');
-
+const newsRouter = require('./routes/news');
 const testimonialsRouter=require("./routes/testimonials")
 
 const app = express();
@@ -28,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/categories', categoriesRouter);
+app.use('/news', newsRouter);
 app.use("/testimonials",testimonialsRouter)
 
 // catch 404 and forward to error handler
