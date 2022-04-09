96acfc9a331d3ee682709505a86a18ea5c6945e2
David <david.vogel.7@gmail.com>
2022-04-09 16:50:30 -0300

    Creation of seeders for Activites on ONG database
    Signed-off-by: David <david.vogel.7@gmail.com>

--- seeders/20220322010814-create-activities.js
+++ seeders/20220322010814-create-activities.js
@@ -9,6 +9,20 @@ module.exports = {
         image: 'urlimagen',       
         createdAt: new Date,
         updatedAt: new Date
+      },
+      {
+        name: 'asdasd',
+        content: 'Contenido ffffff',
+        image: 'urlimagensdasdas',       
+        createdAt: new Date,
+        updatedAt: new Date
+      },
+      {
+        name: 'activity',
+        content: 'Content asdsadasdasds',
+        image: 'urlimage',       
+        createdAt: new Date,
+        updatedAt: new Date
       }], {});
     
   },
