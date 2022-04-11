0be6ed5a01010848e0d3d5b7b4a5000430d848cc
franmassello <franmassello@gmail.com>
2022-04-10 22:01:03 -0300

    add commit

--- controllers/contactoController.js
+++ controllers/contactoController.js
@@ -1,14 +1,22 @@
-'use strict';
-const repository =require('../repositories/contactoRepository');
+"use strict";
+const repository = require("../repositories/contactoRepository");
+const db = require("../models");
 
+exports.updateContacto = async (req, res) => {
+  const id = req.params.id;
+  const updateContacto = repository.updateContacto(req.body, id);
+  if (!updateContacto) {
+    res.status(400).json({ message: "error updating organization" });
+  } else {
+    res.status(200).json("UPDATE");
+  }
+};
 
-exports.updateContacto=async (req,res)=>{
-    const id = req.params.id;
-    
-    const updateContacto=repository.updateContacto(req.body,id);
-    if(!updateContacto){
-        res.status(400).json({ message: "error updating organization"});
-    }else{
-        res.status(200).json('UPDATE');
-    }
-}
+exports.getAllContacts = async (req, res) => {
+  try{
+    const contacts = await db.Contacto.findAll();
+    res.status(200).json(contacts);
+  } catch(err){
+    res.status(400).json({message: "error getting contacts"});
+  }
+};
